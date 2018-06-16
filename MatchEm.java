b3549aa4df15ebf3bb143fcfa4198bdf18d98b22
Neil Patel <nmp61@pitt.edu>
2018-06-16 12:20:20 -0400

    Initial Commit; Basic format for main source file

---
+++ MatchEm.java
@@ -0,0 +1,23 @@
+/*
+*  Author: Neil Patel
+*  GitHub: neilpatel
+*  Project: MatchEm
+*/
+
+// import statements
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.Color;
+import java.awt.BorderlayOut;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JSplitPane;
+
+public class MatchEm extends JFrame {
+	// basic format
+}
